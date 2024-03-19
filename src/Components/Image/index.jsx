925a5f2d846fc68801260b40de6632c28322d33a
nhanbui1512 <buithiennhan0345@gmail.com>
2024-03-19 11:14:21 +0700

    create UI body home

---
+++ src/Components/Image/index.jsx
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+import { useState, forwardRef, useEffect } from "react";
+import images from "../../Assets/images";
+const Image = forwardRef(({ src, alt, ...props }, ref) => {
+  const [fallBack, setFallBack] = useState("");
+
+  useEffect(() => {
+    setFallBack(""); // Reset fallBack when src changes
+  }, [src]);
+
+  const handleErr = () => {
+    setFallBack(images.noImage);
+  };
+  return (
+    <img
+      src={fallBack || src}
+      alt={alt}
+      ref={ref}
+      {...props}
+      onError={handleErr}
+    />
+  );
+});
+
+Image.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  ref: PropTypes.string,
+};
+export default Image;
---
+++ src/Components/SellerList/index.jsx
@@ -0,0 +1,53 @@
+import classNames from "classnames/bind";
+import styles from "./SellerList.module.scss";
+import ProductItem from "../ProductItem";
+import { Link } from "react-router-dom";
+import data from "./data";
+
+const cx = classNames.bind(styles);
+
+function SellerList() {
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("col-left")}>
+        <div className={cx("title-container")}>
+          <h2>Sản phẩm mới nhất</h2>
+        </div>
+        <div>
+          <div
+            style={{
+              padding: 14,
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <div className={cx("link")}>
+              <Link>Thức ăn khô cho chó</Link>
+            </div>
+            <div className={cx("link")}>
+              <Link>Thức ăn mềm, pate cho chó</Link>
+            </div>
+            <div className={cx("link")}>
+              <Link>Xương, bánh thưởng cho chó</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={cx("product-container")}>
+        <div className={cx("product-list")}>
+          {data.map((item, index) => (
+            <ProductItem key={index} data={item} />
+          ))}
+          {/* <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem /> */}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SellerList;
