1fc74970efc9a2e47f2f3eaa1b992bf13d4c7590
linhprovip2002 <dangquangnhatlinh123@gmail.com>
2024-03-20 10:24:09 +0700

    feat/ footer-ui and config tailwind

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,7 +3,7 @@ import style from "./navbar.module.scss";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars,faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(style);
 
@@ -86,6 +86,20 @@ function Navbar() {
             <li>
               <a href="/contact" title="Contact">
                 LIÊN HỆ
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <nav className={cx("nav-icons")}>
+          <ul>
+            <li>
+              <a href="/cart" title="Cart">
+                <FontAwesomeIcon icon={faCartShopping}/>
+              </a>
+            </li>
+            <li>
+              <a href="/user" title="User">
+                <FontAwesomeIcon icon={ faUser } />
               </a>
             </li>
           </ul>
