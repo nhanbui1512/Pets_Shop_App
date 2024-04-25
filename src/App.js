7709f6e8b3d05081b24866b5eda168fe911289fa
nhanbui1512 <buithiennhan0345@gmail.com>
2024-04-25 22:24:12 +0700

    change time auto close of toast

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ function App() {
       <RouterProvider router={router} />
       <ToastContainer
         position="top-right"
-        autoClose={5000}
+        autoClose={2500}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
