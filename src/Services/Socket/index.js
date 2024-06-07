c7af52eaf2c206caed0cd6690e95687189094ce9
nhanbui1512 <buithiennhan0345@gmail.com>
2024-06-07 16:14:32 +0700

    fix url socket

--- src/Services/Socket/index.js
+++ src/Services/Socket/index.js
@@ -1,10 +1,7 @@
 import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : "http://18.142.137.253:3001";
+const URL = "http://18.142.137.253:3001";
 
 export const socket = io(URL, {
   autoConnect: false,
